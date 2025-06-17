ac27d947ab4ee112f6e1079d0f52c6790257aaed
terloo <chensijie08@gmail.com>
2025-06-17 12:42:06 +0800

    chron: 优化wx消息处理逻辑

--- message/consumer.go
+++ message/consumer.go
@@ -2,20 +2,32 @@ package message
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"github.com/terloo/xiaochen/wxbot"
 )
 
 func StartConsumer(ctx context.Context) {
-	for message := range wxbot.StartReceiveMessage(ctx) {
-		for _, h := range handlers {
-			if h.Support(message) {
-				err := h.Handle(ctx, message)
-				if err != nil {
-					log.Printf("handle msg err:%v", err)
+	if err := ctx.Err(); err != nil {
+		return
+	}
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case message := <-wxbot.StartReceiveMessage(ctx):
+			for _, h := range handlers {
+				if h.Support(message) {
+					err := h.Handle(ctx, message)
+					if err != nil {
+						_ = wxbot.SendMsg(ctx, fmt.Sprintf("处理消息失败: %s", err.Error()), message.Chat)
+						log.Printf("handle msg err:%v", err)
+					}
 				}
 			}
 		}
 	}
+
 }
